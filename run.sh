1341ea1ba9a5b57d0c323229f6efc0bb40661bb5
Tadesse Destaw Belay <tbelay23@cic.ipn.mx>
2025-12-10 16:19:19 +0000

    updates

--- run.sh
+++ run.sh
@@ -1,26 +1,8 @@
-llamafactory-cli train examples/train_full/llama3_lora_sft_math.yaml
-
-llamafactory-cli export examples/merge_lora/llama3_full_pt.yaml
-
-llamafactory-cli train examples/train_full/gemma3_full_sft.yaml
-
-llamafactory-cli export examples/merge_lora/gemma3_full_sft.yaml
-
-
-llamafactory-cli train examples/train_lora/gemma3_full_pretrain.yaml
-
-llamafactory-cli export examples/merge_lora/gemma3_full_sft.yaml
-
-llamafactory-cli export examples/merge_lora/gemma3_lora_sft.yaml
-
-llamafactory-cli train examples/train_lora/pt.yaml
-
-llamafactory-cli export examples/merge_lora/llama3_full_pt.yaml
-
-llamafactory-cli train examples/train_lora/llama3_lora_sft_math-all.yaml
-
-llamafactory-cli train examples/train_lora/llama3_lora_sft_math-eng.yaml
-
-llamafactory-cli train examples/train_lora/llama3_lora_sft_math-localized.yaml
-
-llamafactory-cli train examples/train_lora/llama3_lora_sft_math-transalted.yaml
+llamafactory-cli train examples/train_lora/dpo/llama3_dpo_llama_rllama.yaml
+llamafactory-cli export examples/merge_lora/dpo/llama3_dpo_llama_rllama.yaml
+llamafactory-cli train examples/train_lora/dpo/llama3_dpo_llama_rgemma.yaml
+llamafactory-cli export examples/merge_lora/dpo/llama3_dpo_llama_rgemma.yaml
+llamafactory-cli train examples/train_lora/dpo/llama3_dpo_gemma_rllama.yaml
+llamafactory-cli export examples/merge_lora/dpo/llama3_dpo_gemma_rllama.yaml
+llamafactory-cli train examples/train_lora/dpo/llama3_dpo_gemma_rgemma.yaml
+llamafactory-cli export examples/merge_lora/dpo/llama3_dpo_gemma_rgemma.yaml
