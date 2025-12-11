b1860dfab5d86f7396913c03f7dbe745d74c08ac
Tadesse Destaw Belay <tadesseit@gmail.com>
2025-12-11 16:30:35 -0500

    Remove duplicate llama3_dpo_llama_rgemma commands

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 llamafactory-cli train examples/train_lora/dpo/llama3_dpo_llama_rllama.yaml
 llamafactory-cli export examples/merge_lora/dpo/llama3_dpo_llama_rllama.yaml
-llamafactory-cli train examples/train_lora/dpo/llama3_dpo_llama_rgemma.yaml
-llamafactory-cli export examples/merge_lora/dpo/llama3_dpo_llama_rgemma.yaml
 llamafactory-cli train examples/train_lora/dpo/llama3_dpo_gemma_rllama.yaml
 llamafactory-cli export examples/merge_lora/dpo/llama3_dpo_gemma_rllama.yaml
+llamafactory-cli train examples/train_lora/dpo/llama3_dpo_llama_rgemma.yaml
+llamafactory-cli export examples/merge_lora/dpo/llama3_dpo_llama_rgemma.yaml
 llamafactory-cli train examples/train_lora/dpo/llama3_dpo_gemma_rgemma.yaml
 llamafactory-cli export examples/merge_lora/dpo/llama3_dpo_gemma_rgemma.yaml
