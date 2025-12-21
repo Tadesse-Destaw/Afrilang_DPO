1160dcca51a7c2894162492393c99fa19fbbab67
Belay Destaw <tadesse.d@opennlplabs.org>
2025-12-21 21:32:25 +0000

    updates

--- run.sh
+++ run.sh
@@ -8,7 +8,19 @@
 # llamafactory-cli export examples/merge_lora/dpo/llama3_dpo_gemma_rgemma.yaml
 
 # SFT
-llamafactory-cli train examples/train_lora/llama3_lora_sft.yaml
-llamafactory-cli export examples/merge_lora/llama3_lora_sft.yaml
-llamafactory-cli train examples/train_lora/gemma3_lora_sft.yaml
-llamafactory-cli export examples/merge_lora/gemma3_lora_sft.yaml
+# llamafactory-cli train examples/train_lora/llama3_lora_sft.yaml
+# llamafactory-cli export examples/merge_lora/llama3_lora_sft.yaml
+# llamafactory-cli train examples/train_lora/gemma3_lora_sft.yaml
+# llamafactory-cli export examples/merge_lora/gemma3_lora_sft.yaml
+
+llamafactory-cli train examples/train_lora/dpo/llama3_70b_dpo_llama_rllama.yaml
+llamafactory-cli export examples/merge_lora/dpo/llama3_70b_dpo_llama_rllama.yaml
+
+llamafactory-cli train examples/train_lora/llama3_70b_lora_sft.yaml
+llamafactory-cli export examples/merge_lora/llama3_70b_lora_sft.yaml
+
+llamafactory-cli train examples/train_lora/dpo/llama3_dpo_gemma_27b_rllama.yaml
+llamafactory-cli export examples/merge_lora/dpo/llama3_dpo_gemma_27b_rllama.yaml
+
+llamafactory-cli train examples/train_lora/gemma3_27b_lora_sft.yaml
+llamafactory-cli export examples/merge_lora/gemma3_27b_lora_sft.yaml
