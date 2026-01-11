095e9f3a0f58a8bbdb05769248e51660b7d239f1
Belay Destaw <tadesse.d@opennlplabs.org>
2026-01-11 03:19:57 +0000

    update

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 # dpo param efficient
-llamafactory-cli train examples/train_lora/dpo/llama3_dpo_llama_rllama.yaml
-llamafactory-cli export examples/merge_lora/dpo/llama3_dpo_llama_rllama.yaml
+# llamafactory-cli train examples/train_lora/dpo/llama3_dpo_llama_rllama.yaml
+# llamafactory-cli export examples/merge_lora/dpo/llama3_dpo_llama_rllama.yaml
 
 # llamafactory-cli train examples/train_lora/dpo/llama3_dpo_llama_rgemma.yaml
 # llamafactory-cli export examples/merge_lora/dpo/llama3_dpo_llama_rgemma.yaml
 
-llamafactory-cli train examples/train_lora/dpo/llama3_dpo_gemma_rllama.yaml
-llamafactory-cli export examples/merge_lora/dpo/llama3_dpo_gemma_rllama.yaml
+# llamafactory-cli train examples/train_lora/dpo/llama3_dpo_gemma_rllama.yaml
+# llamafactory-cli export examples/merge_lora/dpo/llama3_dpo_gemma_rllama.yaml
 
 # llamafactory-cli train examples/train_lora/dpo/llama3_dpo_gemma_rgemma.yaml
 # llamafactory-cli export examples/merge_lora/dpo/llama3_dpo_gemma_rgemma.yaml
@@ -28,10 +28,10 @@ llamafactory-cli export examples/merge_lora/dpo/llama3_dpo_gemma_rllama.yaml
 # llamafactory-cli export examples/merge_lora/dpo/gemma3-12b-sft-dpo_llama-3-1-8b-it.yaml
 
 # SFT
-# llamafactory-cli train examples/train_lora/llama3_lora_sft.yaml
-# llamafactory-cli export examples/merge_lora/llama3_lora_sft.yaml
-# llamafactory-cli train examples/train_lora/gemma3_lora_sft.yaml
-# llamafactory-cli export examples/merge_lora/gemma3_lora_sft.yaml
+llamafactory-cli train examples/train_lora/llama3_lora_sft.yaml
+llamafactory-cli export examples/merge_lora/llama3_lora_sft.yaml
+llamafactory-cli train examples/train_lora/gemma3_lora_sft.yaml
+llamafactory-cli export examples/merge_lora/gemma3_lora_sft.yaml
 
 # llamafactory-cli train examples/train_lora/dpo/llama3_70b_dpo_llama_rllama.yaml
 # llamafactory-cli export examples/merge_lora/dpo/llama3_70b_dpo_llama_rllama.yaml
